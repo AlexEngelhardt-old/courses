b20f6ea3913ce87093272d7222025c0d448aea7a
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2020-11-14 10:30:32 +0100

    week 1 assignment done

--- Data Structures and Algorithms/02 Data Structures/Week 1 - Basic Data Structures/x5_max_sliding_window.py
+++ Data Structures and Algorithms/02 Data Structures/Week 1 - Basic Data Structures/x5_max_sliding_window.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 
 def max_sliding_window_naive(sequence, m):
     maximums = []
@@ -7,8 +9,55 @@ def max_sliding_window_naive(sequence, m):
     return maximums
 
 
+class QueueWithMax:
+    """To be able to use the linear-time max() operation from exercise 4, we need to use stacks.
+    Fortunately we can implement a queue using two stacks:
+    https://www.geeksforgeeks.org/queue-using-stacks/
+
+    Bad tho: I think this makes the complexity O(n*m) again, worst case at least(?)
+    """
+    def __init__(self):
+        self.stack1 = deque()
+        self.stack2 = deque()
+        self.max_stack1 = deque()
+        self.max_stack2 = deque()
+
+    def enqueue(self, a):
+        self.stack1.append(a)
+        if not self.max_stack1 or a >= self.max_stack1[-1]:  # must be __geq__, because we can have duplicate maxima, then keep previous ones!
+            self.max_stack1.append(a)
+
+    def dequeue(self):
+        if not self.stack1 and not self.stack2:
+            raise IndexError("Queue is empty")
+        if not self.stack2:
+            while self.stack1:
+                a = self.stack1.pop()
+                self.stack2.append(a)
+                if not self.max_stack2 or a >= self.max_stack2[-1]:
+                    self.max_stack2.append(a)
+            self.max_stack1.clear()
+        last = self.stack2.pop()
+        if self.max_stack2 and last == self.max_stack2[-1]:
+            self.max_stack2.pop()
+
+    def max(self):
+        if not self.max_stack1:
+            return self.max_stack2[-1]
+        elif not self.max_stack2:
+            return self.max_stack1[-1]
+        else:
+            return max(self.max_stack1[-1], self.max_stack2[-1])
+
+
 def max_sliding_window(sequence, m):
-    return [1, 2, 3]
+    q = QueueWithMax()
+    for i, x_i in enumerate(sequence):
+        q.enqueue(x_i)
+        if (i+1) > m:
+            q.dequeue()
+        if (i+1) >= m:  # if first 4 elements are processed
+            yield q.max()
 
 
 if __name__ == '__main__':
@@ -17,5 +66,5 @@ if __name__ == '__main__':
     assert len(input_sequence) == n
     window_size = int(input())
 
-    print(*max_sliding_window_naive(input_sequence, window_size))
+    print(*max_sliding_window(input_sequence, window_size))
 
--- Data Structures and Algorithms/02 Data Structures/Week 1 - Basic Data Structures/x5_test.py
+++ Data Structures and Algorithms/02 Data Structures/Week 1 - Basic Data Structures/x5_test.py
@@ -4,4 +4,4 @@ sequence = [2, 7, 3, 1, 5, 2, 6, 2]
 m = 4
 
 print(max_sliding_window_naive(sequence, m))
-print(max_sliding_window(sequence, m))
+print(list(max_sliding_window(sequence, m)))
