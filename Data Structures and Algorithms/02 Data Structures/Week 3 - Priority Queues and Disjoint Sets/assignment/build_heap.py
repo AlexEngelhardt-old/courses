b032fef3aa4296e010d2d1bd2a9905e9e0c25b52
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2020-11-18 11:25:00 +0100

    Min-heap exercise seems to work except for output formatting

--- Data Structures and Algorithms/02 Data Structures/Week 3 - Priority Queues and Disjoint Sets/assignment/build_heap.py
+++ Data Structures and Algorithms/02 Data Structures/Week 3 - Priority Queues and Disjoint Sets/assignment/build_heap.py
@@ -1,7 +1,4 @@
-# python3
-
-
-def build_heap(data):
+def build_min_heap_naive(data):
     """Build a heap from ``data`` inplace.
 
     Returns a sequence of swaps performed by the algorithm.
@@ -21,17 +18,38 @@ def build_heap(data):
     return swaps
 
 
-def main():
+def sift_down(data, i):
+    left_child_i = i * 2 + 1
+    right_child_i = i * 2 + 2
+    new_i = i
+    if left_child_i < len(data):
+        if data[new_i] > data[left_child_i]:
+            new_i = left_child_i
+    if right_child_i < len(data):
+        if data[new_i] > data[right_child_i]:
+            new_i = right_child_i
+    if new_i != i:
+        data[i], data[new_i] = data[new_i], data[i]
+        return [(i, new_i)] + sift_down(data, new_i)
+    return []
+
+
+def build_min_heap(data):
+    # SiftDown for all n nodes (bzw. those that have at least one child)
+    swaps = []
+    first_parent = (len(data)-2) // 2
+    for i in range(first_parent, -1, -1):
+        swaps.append(sift_down(data, i))
+    return swaps
+
+
+if __name__ == "__main__":
     n = int(input())
     data = list(map(int, input().split()))
     assert len(data) == n
 
-    swaps = build_heap(data)
+    swaps = build_min_heap(data)
 
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
-if __name__ == "__main__":
-    main()
---
+++ Data Structures and Algorithms/02 Data Structures/Week 3 - Priority Queues and Disjoint Sets/assignment/build_heap_test.py
@@ -0,0 +1,11 @@
+from build_heap import build_min_heap_naive, build_min_heap
+
+data = [5, 4, 3, 2, 1]
+swaps = build_min_heap(data)
+print(swaps)
+
+print('*' * 40)
+
+data = [1, 2, 3, 4, 5]
+swaps = build_min_heap(data)
+print(swaps)
