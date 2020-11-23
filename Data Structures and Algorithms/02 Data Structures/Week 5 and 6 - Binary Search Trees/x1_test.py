4be2ee46927341989b0b3b621e18038ee0ed1221
Alexander Engelhardt <alexander.w.engelhardt@gmail.com>
2020-11-23 09:47:40 +0100

    Week 6 x1 done, but not pretty

---
+++ Data Structures and Algorithms/02 Data Structures/Week 5 and 6 - Binary Search Trees/x1_test.py
@@ -0,0 +1,10 @@
+from x1_tree_orders import TreeOrders
+
+T = TreeOrders()
+T.key = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+T.left = [7, -1, -1, 8, 3, -1, 1, 5, -1, -1]
+T.right = [2, -1, 6, 9, -1, -1, -1, 4, -1, -1]
+
+print(T.inOrder())
+print(T.preOrder())
+print(T.postOrder())
